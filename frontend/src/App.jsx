c0623b60244d38e122fb7e68534a617310049429
RizkiHibatuloh <rizkihibatuloh0123@gmail.com>
2025-06-01 20:18:10 +0700

    add Tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+       <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-600">TailwindCSS Aktif! 🚀</h1>
+      <p className="text-gray-600 mt-4">Sekarang kamu bisa mulai ngoding frontend-nya 😎</p>
+    </div>
+    </>
+  )
+}
+
+export default App
